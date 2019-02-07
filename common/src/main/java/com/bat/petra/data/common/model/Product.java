42eb66ebca7855345c40f750b457ca2e27d83dbf
Santiago Liscovsky <santolisco@hotmail.com>
2019-02-07 00:27:26 +0000

    align prop

--- common/src/main/java/com/bat/petra/data/common/model/Product.java
+++ common/src/main/java/com/bat/petra/data/common/model/Product.java
@@ -21,21 +21,6 @@ public class Product implements Serializable {
     @Column(name = "productcode")
     private String productId;
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getProductId() {
-        return productId;
-    }
-
-    public void setProductId(String productId) {
-        this.productId = productId;
-    }
 
 
 }
