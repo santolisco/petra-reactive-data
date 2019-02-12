d81353e913f0a45e53c514dad27a0a1fcb9c931c
Santiago Liscovsky <santolisco@hotmail.com>
2019-02-12 22:23:51 +0000

    loading

--- src/main/java/com/bat/petra/data/WorkerApplication.java
+++ src/main/java/com/bat/petra/data/WorkerApplication.java
@@ -1,6 +1,6 @@
 package com.bat.petra.data;
 
-import com.bat.petra.data.service.ReceiveMsgComponent;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/bat/petra/data/service/ReceiveMsgComponent.java
+++ src/main/java/com/bat/petra/data/service/ReceiveMsgComponent.java
@@ -3,14 +3,13 @@ package com.bat.petra.data.service;
 import com.bat.petra.data.model.*;
 
 import org.json.JSONException;
-import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 
-
 @Component
-
+@EnableRabbit
 public class ReceiveMsgComponent {
 
 
@@ -18,9 +17,6 @@ public class ReceiveMsgComponent {
         public void receiveMessage(PetraMessage message){
             try {
                  System.out.print(" receive message [" + message.toString() + "] ");
-
-
-
 
         } catch (JSONException e) {
             e.printStackTrace();
